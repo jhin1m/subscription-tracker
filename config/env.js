958f99c3f94bfe0174c8e3169212a3970ac36a4b
jhin1m <ducanhfake@gmail.com>
2025-04-29 00:14:18 +0700

    Config done!

---
+++ config/env.js
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+
+config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
+//explain this
+//this is a config function that loads the environment variables from the .env.development.local file
+//if the NODE_ENV is not set, it will use the development environment
+//if the NODE_ENV is set to production, it will use the .env.production.local file
+//if the NODE_ENV is set to test, it will use the .env.test.local file
+//if the NODE_ENV is not set, it will use the .env.development file
+
+
+export const {PORT, NODE_ENV } = process.env;
+
